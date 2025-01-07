0c0c1f347170e48e798548a71b0aa9a00305a136
duong.vd <duong.vd@ptf.com.vn>
2025-01-07 16:24:22 +0700

    add config cors

---
+++ trello/src/main/java/com/example/trello/Config/CORSConfig.java
@@ -0,0 +1,20 @@
+package com.example.trello.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
